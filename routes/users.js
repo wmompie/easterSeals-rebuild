e12b0a7b6a530f4eedab8093836a4da9d8174c07
wmompie <wmompie@gmail.com>
2019-06-20 00:17:42 -0400

    User schema model and validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Registers a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please add name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('address', 'Please include your address')
+      .not()
+      .isEmpty(),
+    check('city', 'Please include your city')
+      .not()
+      .isEmpty(),
+    check('state', 'Please choose your state')
+      .not()
+      .isEmpty(),
+    check('postal', 'Please include your postal code')
+      .not()
+      .isEmpty(),
+    check('phone', 'Please include your phone number')
+      .not()
+      .isEmpty(),
+    check('distance', 'Please choose a distance')
+      .not()
+      .isEmpty(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect DataBase
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({ msg: 'Hello World, welcome.' }));
 
 // Define Routes
