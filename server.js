2b8b216039eb90b3c5c66d9f8e5eb46ff384b7c4
wmompie <wmompie@gmail.com>
2019-06-19 23:31:46 -0400

    added users backend route

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.get('/', (req, res) => res.json({ msg: 'Hello World, welcome.' }));
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
