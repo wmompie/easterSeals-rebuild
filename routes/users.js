0b1d96a9c9e109a268fd48dae358a313a7ac6c7b
wmompie <wmompie@gmail.com>
2019-06-23 12:10:25 -0400

    register route, create & respond with JWT

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -33,13 +35,49 @@ router.post(
       .not()
       .isEmpty(),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, address, address2, city, state, postal, phone, distance } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'A user with that email has already registered' });
+      }
+
+      user = new User({
+        name,
+        email,
+        address,
+        address2,
+        city,
+        state,
+        postal,
+        phone,
+        distance,
+      });
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, config.get('jwtSecret'), (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
